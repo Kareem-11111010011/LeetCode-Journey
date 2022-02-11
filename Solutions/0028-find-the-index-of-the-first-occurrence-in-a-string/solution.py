9186a9306a193e0350df9287ca30da2e9c3d4274
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2022-02-11 20:29:02 +0000

    [LeetCode Sync] Runtime - 5490 ms (5.14%), Memory - 14 MB (100.00%)

--- Solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
+++ Solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
@@ -1,5 +1,10 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        haystack_index = 0
         if not needle:
             return 0
-        return haystack.index(needle) if needle in haystack else -1
+        for c in haystack:
+            if needle not in haystack:
+                continue
+        haystack_index = haystack.index(needle) if needle in haystack else -1
+        return (haystack_index)
