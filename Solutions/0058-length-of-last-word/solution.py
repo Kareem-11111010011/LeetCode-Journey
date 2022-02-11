68267067c84ccdc3ee3882e64f815637700d0106
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2022-02-11 23:24:45 +0000

    [LeetCode Sync] Runtime - 36 ms (0.31%), Memory - 13.9 MB (100.00%)

--- Solutions/0058-length-of-last-word/solution.py
+++ Solutions/0058-length-of-last-word/solution.py
@@ -1,3 +1,7 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return 0 if not s else len(s.split()[-1])
+        for count,char in enumerate(reversed(s.strip()), 1):
+            if char == " ":
+                count = count - 1
+                break
+        return count
