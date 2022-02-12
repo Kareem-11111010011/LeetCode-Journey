fb5ffcaf68d51eeab61107b984126b14f5e5df42
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2022-02-12 22:32:49 +0000

    [LeetCode Sync] Runtime - 34 ms (0.24%), Memory - 13.9 MB (100.00%)

--- Solutions/0027-remove-element/solution.py
+++ Solutions/0027-remove-element/solution.py
@@ -1,5 +1,6 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         while val in nums:
-            nums.remove(val)
-        return len(nums)
+            del nums[nums.index(val)]
+        return (len(nums))
+            
