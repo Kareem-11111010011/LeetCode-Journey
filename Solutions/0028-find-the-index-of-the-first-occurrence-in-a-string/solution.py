06e2b5f1cef3be90d95835d0abac1ce29d8cd50f
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2022-02-13 18:04:46 +0000

    [LeetCode Sync] Runtime - 33 ms (5.14%), Memory - 14.2 MB (100.00%)

--- Solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
+++ Solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
@@ -3,8 +3,5 @@ class Solution:
         haystack_index = 0
         if not needle:
             return 0
-        for c in haystack:
-            if needle not in haystack:
-                continue
         haystack_index = haystack.index(needle) if needle in haystack else -1
         return (haystack_index)
