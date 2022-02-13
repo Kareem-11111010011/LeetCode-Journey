8bbed396e93b02fd5d811a6d4ea28e60f4449f01
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2022-02-13 22:52:23 +0000

    [LeetCode Sync] Runtime - 36 ms (5.26%), Memory - 14.8 MB (100.00%)

--- Solutions/0125-valid-palindrome/solution.py
+++ Solutions/0125-valid-palindrome/solution.py
@@ -1,4 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        alphanumeric = "".join(char for char in s if char.isalnum()).lower()
-        return (alphanumeric == alphanumeric[::-1])
+        alpha_numeric_s = "".join(filter(str.isalnum, s))
+        return (alpha_numeric_s.lower() == alpha_numeric_s[::-1].lower())
