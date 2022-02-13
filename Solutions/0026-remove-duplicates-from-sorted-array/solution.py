6c0800f36a68fe21ff2faaf63a871e9df5a39763
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2022-02-13 17:27:23 +0000

    [LeetCode Sync] Runtime - 179 ms (5.29%), Memory - 15.7 MB (100.00%)

--- Solutions/0026-remove-duplicates-from-sorted-array/solution.py
+++ Solutions/0026-remove-duplicates-from-sorted-array/solution.py
@@ -3,7 +3,8 @@ class Solution:
         count = 1
         while count < len(nums):
             if nums[count] == nums[count - 1]:
-                del [nums[count]]
+                del nums[count]
             else:
                 count += 1
         return len(nums)
+            
