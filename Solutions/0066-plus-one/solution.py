003739db37643a5133c45c2dd68b2706bcb8f220
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2022-02-19 20:24:51 +0000

    [LeetCode Sync] Runtime - 75 ms (0.05%), Memory - 13.8 MB (100.00%)

--- Solutions/0066-plus-one/solution.py
+++ Solutions/0066-plus-one/solution.py
@@ -1,4 +1,4 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        answer = int("".join([str(ints) for ints in digits])) + 1
-        return [digit for digit in str(answer)]
+        integer_form = int("".join([str(digit) for digit in digits])) + 1
+        return [integer for integer in str(integer_form)]
