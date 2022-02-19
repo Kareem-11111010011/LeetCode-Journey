2a9da1b55cc63c2e16cd337c2f9dbc775f801a02
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2022-02-19 23:01:38 +0000

    [LeetCode Sync] Runtime - 40 ms (0.38%), Memory - 15 MB (100.00%)

--- Solutions/0412-fizz-buzz/solution.py
+++ Solutions/0412-fizz-buzz/solution.py
@@ -1,11 +1,21 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        fizz_buzz_list = list(range(1, n + 1))
-        for count, num in enumerate(fizz_buzz_list):
-            if (num % 3 == 0) & (num % 5 == 0):
-                fizz_buzz_list[count] = "FizzBuzz"
-            elif num % 3 == 0:
-                fizz_buzz_list[count] = "Fizz"
-            elif num % 5 == 0:
-                fizz_buzz_list[count] = "Buzz"
-        return [str(fizz_buzz) for fizz_buzz in fizz_buzz_list]
+        output = []
+        Fizz_Buzz = 15
+        Fizz = 3
+        Buzz = 5
+        for num in range(1, n + 1):
+            if num == Fizz_Buzz:
+                output.append("FizzBuzz")
+                Fizz += 3
+                Buzz += 5
+                Fizz_Buzz += 15
+            elif num == Fizz:
+                output.append("Fizz")
+                Fizz += 3
+            elif num == Buzz:
+                output.append("Buzz")
+                Buzz += 5
+            else:
+                output.append(str(num))
+        return output
