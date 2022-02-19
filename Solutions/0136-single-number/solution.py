03fdcb7f5a35f6ac5d168e79b05ee6991ab27861
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2022-02-19 22:23:46 +0000

    [LeetCode Sync] Runtime - 155 ms (9.42%), Memory - 16.7 MB (100.00%)

--- Solutions/0136-single-number/solution.py
+++ Solutions/0136-single-number/solution.py
@@ -1,13 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        index = 0
-        nums = sorted(nums)
-        while True:
-            if len(nums) == 1:
-                return nums[index]
-            elif nums[index] == nums[index + 1]:
-                nums.pop(index)
-                nums.pop(index)
-            else:
-                return nums[index]
-                
+        output = 0
+        for num in nums:
+            output ^= num
+        return output
+    
