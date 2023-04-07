fd075e0ef4246acaf0309b351442490ae5347ce1
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2023-04-07 18:37:06 +0000

    [LeetCode Sync] Runtime - 85 ms (6.09%), Memory - 42.8 MB (100.00%)

--- Solutions/0001-two-sum/solution.java
+++ Solutions/0001-two-sum/solution.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] soln = {0,0};
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target && (i != j)) {
-                    soln[0] = i; 
-                    soln[1] = j;
-                    break;
+        for(int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
                 }
             }
         }
-        return soln;
+        return new int[]{0,0};
     }
 }
