4f525b0efea2cf13ca35696df267fdf5fb75a6b4
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2026-05-23 16:24:53 +0000

    [LeetCode Sync] Runtime - 3 ms (48.34%), Memory - 23.4 MB (45.60%)

--- Solutions/0344-reverse-string/solution.py
+++ Solutions/0344-reverse-string/solution.py
@@ -3,6 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        for char in s[::-1]:
-            s.pop(0)
-            s.append(char)
+        
+        i = 0
+        j = len(s) - 1
+        
+        while i < j:
+            temp = s[i]
+            s[i] = s[j]
+            s[j] = temp
+            i += 1
+            j -= 1
