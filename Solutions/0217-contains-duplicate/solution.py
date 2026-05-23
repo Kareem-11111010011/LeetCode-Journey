dc30d842709c40a9a152db2a0b520e2bbf9160d6
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2026-05-23 21:37:43 +0000

    [LeetCode Sync] Runtime - 8 ms (78.29%), Memory - 31.3 MB (59.83%)

--- Solutions/0217-contains-duplicate/solution.py
+++ Solutions/0217-contains-duplicate/solution.py
@@ -1,11 +1,10 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        n = {nums[i]:0 for i in range(len(nums))}
+        seen = set()
 
-        for i in range(len(nums)):
-            if n[nums[i]] + 1 >= 2: return True
-            n[nums[i]] += 1
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
         
         return False
-
-
