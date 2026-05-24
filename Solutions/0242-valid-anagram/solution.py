9194528ed2c9876ee5ad3a65d1d318f5dfb99640
Kareem-11111010011 <70543786+Kareem-11111010011@users.noreply.github.com>
2026-05-24 00:31:14 +0000

    [LeetCode Sync] Runtime - 11 ms (76.55%), Memory - 19.4 MB (75.07%)

--- Solutions/0242-valid-anagram/solution.py
+++ Solutions/0242-valid-anagram/solution.py
@@ -1,17 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        flag1 = {}
-        flag2 = {}
-        
-        for char in s:
-            if char in flag1:
-                flag1[char] += 1
-            else:
-                flag1[char] = 1
-        for char in t:
-            if char in flag2:
-                flag2[char] += 1
-            else:
-                flag2[char] = 1
+        if len(s) != len(t):
+            return False
 
-        return False if flag1 != flag2 else True
+        sd = {}
+        for char in s:
+            sd[char] = sd.get(char, 0) + 1
+        
+        td = {}
+        for char in t:
+            td[char] = td.get(char, 0) + 1
+        
+        return sd == td
